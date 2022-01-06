0aaedefda0ad8c1a87d083a085a51f2b99b05406
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2022-01-06 14:31:53 +0000

    Adds jump functionality

--- chara.js
+++ chara.js
@@ -1,29 +1,35 @@
+import { incrementCustomProperty, getCustomProperty, setCustomProperty } from "./updateCustomProperty.js"
+
 const chara = document.querySelector("[data-chara]")
 const JUMP_SPEED = 0.45
-const GRAVITY = 0.011
+const GRAVITY = 0.0015
 const CHARA_FRAME_COUNT = 2
 const FRAME_TIME = 100
 
 let isJumping = false
 let charaFrame = 0
 let currentFrameTime = 0
+let yVelocity = 0
 
 export function setupChara() {
   isJumping = false
   charaFrame = 0
   currentFrameTime = 0
+  document.removeEventListener("keydown", onJump)
+  document.addEventListener("keydown", onJump)
 }
 
 export function updateChara(delta, speedScale) {
   handleRun(delta, speedScale)
-  handleJump()
-}
-
+  handleJump(delta)
+} 
+ 
 function handleRun(delta, speedScale) {
   if (isJumping) {
-    chara.src = `img/chara-stationary.png`
+    chara.src = `imgs/chara-stationary.png`
     return
   }
+
   if (currentFrameTime >= FRAME_TIME) {
     charaFrame = (charaFrame + 1) % CHARA_FRAME_COUNT
     chara.src = `imgs/chara-run-${charaFrame}.png`
@@ -32,6 +38,18 @@ function handleRun(delta, speedScale) {
   currentFrameTime += delta * speedScale
 }
 
-function handleJump() {
+function handleJump(delta) {
+  if (!isJumping) return
+  incrementCustomProperty(chara, "--bottom", yVelocity * delta)
+  if (getCustomProperty(chara, "--bottom") <= 0) {
+    setCustomProperty(chara, "--bottom", 0)
+    isJumping = false
+  }
+  yVelocity -= GRAVITY * delta
+}
 
-} 
+function onJump(e) {
+  if (e.code !== "Space" || isJumping) return
+  yVelocity = JUMP_SPEED
+  isJumping = true
+}
