49745f2580134ad90aacdda9dffc318a40ae5e18
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2022-01-06 13:08:26 +0000

    Hides text on start

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const WORLD_HEIGHT = 30
 const SPEED_SCALE_INCREASE = 0.00001
 
 const worldElem = document.querySelector("[data-world]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
@@ -12,6 +14,7 @@ document.addEventListener("keydown", handleStart, { once: true })
 
 let lastTime
 let speedScale
+let score
 
 function update(time) {
   if (lastTime == null) {
@@ -23,19 +26,27 @@ function update(time) {
 
   updateGround(delta, speedScale)
   updateSpeedScale(delta)
+  updateScore(delta)
 
   lastTime = time
   window.requestAnimationFrame(update)
 }
 
-function undateSpeedScale(delta) {
+function updateSpeedScale(delta) {
   speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta) {
+  score += delta * 0.01
+  scoreElem.textContent = Math.floor(score)
 }
 
 function handleStart() {
   lastTime = null
   speedScale = 1
+  score = 0
   setupGround()
+  startScreenElem.classList.add("hide")
   window.requestAnimationFrame(update)
 }
 
